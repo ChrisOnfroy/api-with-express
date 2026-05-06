b3d868dc83afd7ecd45aa2515ba7f1b80426f9fb
Cristian Morales <cmoralesavendano6@gmail.com>
2026-05-06 18:21:31 -0500

    se agg users

--- src/server.js
+++ src/server.js
@@ -5,9 +5,26 @@ const PORT = 3000;
 
 app.get('/', (req, res) => {
     res.json({
-        nombre: "Juan",
-        edad: 25,
-        ciudad: "Madrid"
+       user : [
+        {   
+            id:2,
+            nombre: "Juan",
+            edad: 25,
+            ciudad: "Madrid"
+        },
+        {   
+            id:3,
+            nombre: "David",
+            edad: 32,
+            ciudad: "Valencia"
+        },
+        {   
+            id:5,
+            nombre: "Anibal",
+            edad: 43,
+            ciudad: "Barcelona"
+        }
+       ]
     })
 });
 
